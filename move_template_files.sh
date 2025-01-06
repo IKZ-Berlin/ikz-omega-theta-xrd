92dc1c343b420ce295ae2f760c39808910f20c7b
budschi <budschi@users.noreply.github.com>
2025-01-06 14:12:51 +0000

    chore: accept new Cruft update

--- move_template_files.sh
+++ move_template_files.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
 
-rsync -avh nomad-ikz_omega_theta_xrd/ .
-rm -rfv nomad-ikz_omega_theta_xrd
+if ! command -v rsync >/dev/null 2>&1; then
+  echo "rsync required, but not installed!"
+  exit 1
+else
+  rsync -avh ikz_omega_theta_xrd/ .
+  rm -rfv ikz_omega_theta_xrd
+fi
