bb4f100cd8075e5577109fb56a2d7db4f7c5f484
budschi <budschi@users.noreply.github.com>
2025-01-20 14:12:29 +0000

    chore: accept new Cruft update

--- tests/apps/test_app.py
+++ tests/apps/test_app.py
@@ -1,6 +1,5 @@
 def test_importing_app():
     # this will raise an exception if pydantic model validation fails for th app
-    from nomad_ikz_omega_theta_xrd.apps import myapp
+    from ikz_omega_theta_xrd.apps import app_entry_point
 
-    assert myapp.app.label == 'MyApp'
-
+    assert app_entry_point.app.label == 'NewApp'
